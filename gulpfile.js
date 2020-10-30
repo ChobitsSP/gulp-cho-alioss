20aeea8cac3cef26d9e32438f84c48ca1372c9b0
Chobits <chobits@live.cn>
2020-10-30 18:58:48 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,13 @@ const alias = require('./index.js');
 gulp.task('default', ['test']);
 
 gulp.task('test', () => {
-  return gulp.src('demo/**/*.js')
-    .pipe(alias('./demo', {
-      "@/*": ["app/*"]
+  return gulp.src('demo/**/*.js', { base: 'demo' })
+    .pipe(alias({
+      region: "oss-cn-beijing",
+      accessKeyId: "accessKeyId",
+      accessKeySecret: "accessKeySecret",
+      bucket: "chobits",
+      prefix: 'test1',
     }))
     .pipe(gulp.dest('./dist'));
 });
